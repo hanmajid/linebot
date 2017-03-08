03a10dda3efbe28f8bf5e7b6055ead2a9292be99
harryawk <harry.kambey@gmail.com>
2017-03-08 10:32:44 +0700

    add line sdk

--- app.py
+++ app.py
@@ -1,26 +1,45 @@
-from flask import Flask, url_for
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
 app = Flask(__name__)
-counter = 0
 
-@app.route('/')
-def api_root():
-    return 'Welcome'
+line_bot_api = LineBotApi('BanLnTCb802Zvxu88pspr3aL6SsOIXG9FLFY3E1drgaLsJCqfJ/X48TLgAIIWEpEz4nbw4u4sohV3ZS5G9Dt4p36kOXQ4ILbf339+C6wrwM/uEMMBA6jWKrrBrXn4dDLWc5MHltB/wdr9eRnVLSmagdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('ab4a046624a52e60f5a3ffe253be1729')
 
-@app.route('/articles')
-def api_articles():
-    return 'List of ' + url_for('api_articles')
 
-@app.route('/articles/<articleid>')
-def api_article(articleid):
-    return 'You are reading ' + articleid
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
-@app.route('/send', methods=['GET','POST'])
-def api_receive():
-	global counter
-	counter += 1
-	return 'Added to list, counter : ' + str(counter)
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-if __name__ == '__main__':
-	# counter = 0
-	print "counter : " + str(counter)
-	app.run()
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+    app.run()
